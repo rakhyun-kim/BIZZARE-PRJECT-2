df753e052304191ec7c089a377f4abfa544bb130
RAKHYUN <kyle2598@gmail.com>
2025-02-07 16:44:47 -0600

    Initial Commit

---
+++ BIZZARE/BIZZAREApp.swift
@@ -0,0 +1,17 @@
+//
+//  BIZZAREApp.swift
+//  BIZZARE
+//
+//  Created by Rakhyun Kim on 2/7/25.
+//
+
+import SwiftUI
+
+@main
+struct BIZZAREApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
